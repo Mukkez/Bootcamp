05f096d82a5d0c81306db70a6c3dea839cce0828
Mukkez <brick.freaky@brick-store.com>
2022-10-06 20:09:58 +0200

    first commit

--- tasks/Day_51/Lev2_3_js-vertiefung_conditional-statements_difference/assets/js/main.js
+++ tasks/Day_51/Lev2_3_js-vertiefung_conditional-statements_difference/assets/js/main.js
@@ -1,6 +1,5 @@
-console.log('%c----- Nummer 35 -----', 'color: white; background-color: darkgreen');
-function myFunct35() {
-	let a = 35;
+function myFunct(nummer) {
+	let a = nummer;
 	let b;
 	if (a > 27) {
 		b = 2 * (a - 27);
@@ -9,49 +8,7 @@ function myFunct35() {
 		console.log('Nummer ist kleiner als 27');
 	}
 }
-myFunct35();
-
-/*--------------------------------------------------------------------------------*/
-
-console.log('%c----- Nummer 74 -----', 'color: white; background-color: darkgreen');
-function myFunct74() {
-	let a = 74;
-	let b;
-	if (a > 27) {
-		b = 2 * (a - 27);
-		console.log(b);
-	} else {
-		console.log('Nummer ist kleiner als 27');
-	}
-}
-myFunct74();
-
-/*--------------------------------------------------------------------------------*/
-
-console.log('%c----- Nummer 123 -----', 'color: white; background-color: darkgreen');
-function myFunct123() {
-	let a = 123;
-	let b;
-	if (a > 27) {
-		b = 2 * (a - 27);
-		console.log(b);
-	} else {
-		console.log('Nummer ist kleiner als 27');
-	}
-}
-myFunct123();
-
-/*--------------------------------------------------------------------------------*/
-
-console.log('%c----- Nummer 3 -----', 'color: white; background-color: darkgreen');
-function myFunct3() {
-	let a = 3;
-	let b;
-	if (a > 27) {
-		b = 2 * (a - 27);
-		console.log(b);
-	} else {
-		console.log('Nummer ist kleiner als 27');
-	}
-}
-myFunct3();
+myFunct(35);
+myFunct(74);
+myFunct(123);
+myFunct(5);
