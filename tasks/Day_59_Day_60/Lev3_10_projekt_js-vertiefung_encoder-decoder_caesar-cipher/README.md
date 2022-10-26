b6d76d4a154f046d8b2ee91f823477e741f535d1
Mukkez <brick.freaky@brick-store.com>
2022-10-27 00:04:20 +0200

    commit

---
+++ tasks/Day_59_Day_60/Lev3_10_projekt_js-vertiefung_encoder-decoder_caesar-cipher/README.md
@@ -0,0 +1,39 @@
+## Js Vertiefung - Lev3_10_projekt_js-vertiefung_encoder-decoder_caesar-cipher
+
+Eine Übung im SuperCode Bootcamp als Gruppe:
+
+[Pass80](https://github.com/Pass80)
+
+[WeiMing](https://github.com/WeiMingTay)
+
+[Mukkez](https://github.com/Mukkez)
+
+## 🎓 Aufgabe
+
+Die **Caesar-Verschlüsselung** ist ein einfaches symmetrisches Verschlüsselungsverfahren, das auf der monographischen und monoalphabetischen Substitution basiert. Bei der Verschlüsselung wird jeder Buchstabe des Klartexts auf einen Geheimtextbuchstaben abgebildet. **Diese Abbildung ergibt sich, indem man die Zeichen eines geordneten Alphabets um eine bestimmte Anzahl zyklisch nach rechts verschiebt (rotiert)**; zyklisch bedeutet, dass man beim Verschieben über Z hinaus wieder bei A anfangend weiterzählt. Die Anzahl der verschobenen Zeichen bildet den Schlüssel, der für die gesamte Verschlüsselung unverändert bleibt. Beispiel für eine Verschiebung um drei Zeichen.
+
+Beispiel für die Caesar-Verschlüsselung:
+
+```
+Klartext-Alphabet:  a b c d e f g h i j k l m n o p q r s t u v w x y z
+Geheimtextalphabet: D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+Schlüssel: 3
+```
+
+Bei diesem Beispiel wird das Wort **„wikipedia“** als **„ZLNLSHGLD“** verschlüsselt.
+
+Erstelle eine HTML-Seite mit einem Formular, in dem wir zwischen Kodierung und Dekodierung wählen können.
+
+Das Formular hat zwei Eingaben (die Zeichenfolge und den Schlüssel) und eine Ausgabe (das verschlüsselte Ergebnis).
+
+## 💻 Running
+
+Zur Seite —> - [Lev3_10_projekt_js-vertiefung_encoder-decoder_caesar-cipher](https://mukkez.github.io/Bootcamp/tasks/Day_59_Day_60/Lev3_10_projekt_js-vertiefung_encoder-decoder_caesar-cipher/)
+
+<p align="left">
+</p>
+
+<h3 align="left">Languages and Tools:</h3>
+<p align="left"> <a href="https://www.w3schools.com/html/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a>
+<a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> 
+<a href="https://www.w3schools.com/css/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="css3" width="40" height="40"/> </a> </p>
---
+++ tasks/Day_59_Day_60/Lev3_10_projekt_js-vertiefung_encoder-decoder_caesar-cipher/assets/js/main.js
@@ -0,0 +1,93 @@
+// Variablen bestimmen
+
+let userInput = document.getElementById('userInput'),
+	userKey = document.getElementById('userKey'),
+	outputResult = document.getElementById('outputResult'),
+	outputAlphabet = document.getElementById('outputAlphabet'),
+	outputNewAlphabet = document.getElementById('outputNewAlphabet'),
+	outputKey = document.getElementById('outputKey'),
+	newAlphabet = [],
+	op = '',
+	alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Gibt das Alphabet auf der Seite aus.
+outputAlphabet.innerHTML = alphabet
+	.map((b) => {
+		return `<p>${b}</p>`;
+	})
+	.join(' ');
+
+// function Kodieren.
+function btnCheck() {
+	op = '+';
+	newAlphabet = [];
+	let codeIn = userInput.value;
+	outputResult.innerHTML = check(codeIn);
+}
+
+// function Dekodieren.
+function btnChange() {
+	op = '-';
+	newAlphabet = [];
+	userInput.value = outputResult.innerHTML;
+	let codeIn = userInput.value;
+	outputResult.innerHTML = check(codeIn);
+}
+
+// Haupt function
+function check(a) {
+	let key = Number(userKey.value);
+	outputKey.innerHTML = userKey.value;
+	// prüft welcher Rechenoperator zuständig ist.
+	// Bei PLUS neues Alphabet.
+	if (op === '+') {
+		// Ermittelt neues Alphabet mit dem userKey
+		for (let i = 0; i < alphabet.length; i++) {
+			let newArray = (i + key) % alphabet.length;
+			newAlphabet.push(alphabet[newArray]);
+		}
+		// output von dem neuen Alphabet.
+		outputNewAlphabet.innerHTML = newAlphabet
+			.map((b) => {
+				return `<p>${b}</p>`;
+			})
+			.join(' ');
+
+		// bei MINUS normales Alphabet.
+	} else if (op === '-') {
+		outputNewAlphabet.innerHTML = alphabet
+			.map((b) => {
+				return `<p>${b}</p>`;
+			})
+			.join(' ');
+	}
+
+	// wandelt die Wörter (via ASCII-Zeichen) um und beachtet dabei Groß- und Kleinschreibung.
+	return a
+		.split('')
+		.map((txt) => {
+			let lettersNumber = txt.charCodeAt(0);
+			if ('A' <= txt && txt <= 'Z' && op === '+') {
+				lettersNumber = ((lettersNumber - 65 + key) % 26) + 65;
+			} else if ('A' < txt && txt <= 'Z' && op === '-') {
+				lettersNumber = ((lettersNumber - 65 - key) % 26) + 65;
+			} else if ('a' <= txt && txt <= 'z' && op === '+') {
+				lettersNumber = ((lettersNumber - 97 + key) % 26) + 97;
+			} else if ('a' < txt && txt <= 'z' && op === '-') {
+				lettersNumber = ((lettersNumber - 97 - key) % 26) + 97;
+			}
+			return String.fromCharCode(lettersNumber);
+		})
+		.join('');
+}
+
+// versteckt das Alphabet
+function hideShow() {
+	if (outputAlphabet.style.display === 'none') {
+		outputAlphabet.style.display = 'grid';
+		outputNewAlphabet.style.display = 'grid';
+	} else {
+		outputAlphabet.style.display = 'none';
+		outputNewAlphabet.style.display = 'none';
+	}
+}
